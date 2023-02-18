6e00880e360a07f05914803ec98b7747cf478d06
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-02-18 16:19:21 +0530

    calendar and people details

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,9 +7,29 @@ import {
     faTaxi} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "./Header.css";
+import {useState} from 'react';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import {format} from "date-fns";
 
 const Header =()=>{
+    const [openDate,setopenDate] = useState(false);
+    const [openOptions, setopenOptions]=useState(false);
+    const [options, setOptions]= useState({
+        adult:1,
+        children:0,
+        room:0
+    })
+    const [date, setDate] = useState([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection'
+        }
+      ]);
     return(
+
         <div className="header">
             <div className="headerContainer">
             <div className="headerList">
@@ -55,11 +75,45 @@ const Header =()=>{
             </div>
             <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-                <span className="headerSearchText">date to date</span>
+                <span onClick={()=>setopenDate(!openDate)} className="headerSearchText">{`${format(date[0].startDate,"dd/mm/yyyy")} to
+                 ${format(date[0].endDate,"dd/mm/yyyy")}`}</span>
+                {openDate && <DateRange
+                editableDateInputs={true}
+                onChange={item => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+                />}
             </div>
             <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-                <span className="headerSearchText">2 Adults 2 Children 1Room</span>
+                <span  className="headerSearchText">{`${options.adult} Adult . ${options.children} Children . ${options.room} Room`}</span>
+                <div className="options">
+                    <div className="optionItem">
+                        <span className="optionText">Adult</span>
+                        <div className="optionCounter">
+                            <button className="optionCounterButton">-</button>
+                            <span className="optionCounternumber">1</span>
+                            <button className="optionCounterButton">+</button>
+                        </div>
+                    </div>
+                    <div className="optionItem">
+                        <span className="optionText">Children</span>
+                        <div className="optionCounter">
+                            <button className="optionCounterButton">-</button>
+                            <span className="optionCounternumber">0</span>
+                            <button className="optionCounterButton">+</button>
+                        </div>
+                    </div>
+                    <div className="optionItem">
+                        <span className="optionText">Room</span>
+                        <div className="optionCounter">
+                            <button className="optionCounterButton" >-</button>
+                            <span className="optionCounternumber">1</span>
+                            <button className="optionCounterButton">+</button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="headerSearchItem">
                 <button className="headerBtn">Search</button>
