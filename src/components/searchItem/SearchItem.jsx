8d41e39b6a1e5f1f1baf426614f92d29f9bf98f2
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-02-20 15:39:58 +0530

    react slider

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,6 +1,10 @@
 import "./searchItem.css";
 
+
 const SearchItem = () => {
+  
+ 
+
   return (
     <div className="searchItem">
       <img
@@ -31,7 +35,7 @@ const SearchItem = () => {
         <div className="siDetailTexts">
           <span className="siPrice">$112</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <button className="siCheckButton">See availability</button>
+          <button className="siCheckButton" >See availability</button>
         </div>
       </div>
     </div>
